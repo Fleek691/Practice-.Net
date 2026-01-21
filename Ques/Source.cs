1fd71db74f89fb1011fc3cd26d5928ed4ec0af96
Avishek <avishekprasad12345@gmail.com>
2026-01-21 12:35:47 +0530

    M1 Questions

--- Ques/Source.cs
+++ Ques/Source.cs
@@ -1,14 +1,37 @@
-public class Source
+// public class Source
+// {
+//     public int Add(int a,int b,int c)
+//     {
+//         System.Console.WriteLine("Integer");
+//         return a+b+c;
+//     }
+
+//     public double Add(double a,double b,double c)
+//     {
+//         System.Console.WriteLine("Double");
+//         return a+b+c;
+//     }
+// }
+
+
+public class Employee
 {
-    public int Add(int a,int b,int c)
+    public int Id{get;set;}
+    public string Name{get;set;}
+}
+
+
+public class Pract
+{
+    public static void Main(string[] args)
     {
-        System.Console.WriteLine("Integer");
-        return a+b+c;
+        Employee em1=new Employee{Id=1,Name="Avsihek"};
+        Employee em2=new Employee{Id=2,Name="Aditya"};
+        Employee em3=new Employee{Id=3,Name="Aayush"};
+        Employee em4=new Employee{Id=4,Name="Sreekanth"};
+        
+        
     }
 
-    public double Add(double a,double b,double c)
-    {
-        System.Console.WriteLine("Double");
-        return a+b+c;
-    }
+    
 }
